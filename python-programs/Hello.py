2b38b6b34360d7c565cbb6846d566863239b8dc5
Sachin Kudatini <cksachin94@gmail.com>
2026-08-21 23:19:26 +0530

    commit 3

---
+++ python-programs/Hello.py
@@ -0,0 +1,30 @@
+# # print("Hello World !")
+
+# # a  = "Hello This is Sachin !" \
+# # "i Will be the next developer"
+# # print(a)
+# # print(a[3]);
+# # print(a[0]);
+
+# a = "IWILLTHENEXTDEVELOPER";
+# # for String in a:
+#     # print(String)
+# print(a)
+
+# print(len(a))
+
+# # print(a[3])
+# # print(a[-1])  
+# # print(a[1:5])
+# # print(a[2:13])
+
+# a = "  IWILLTHENEXTDEVELOPER  ";    
+# s = "Python is Fun"
+# print(a.strip())
+# print(a.upper())
+# print(f"This is "{a.lower()} )
+# print(s.replace("Fun","Awesome"))
+
+name = "Jake"
+age = 22
+print(f"Name: {name}, Age: {age}")
