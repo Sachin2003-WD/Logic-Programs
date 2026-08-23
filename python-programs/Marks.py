f05959cda932a51186e2013c355de0d3310369c6
Sachin Kudatini <cksachin94@gmail.com>
2026-08-24 00:07:47 +0530

    commit 6

--- python-programs/Marks.py
+++ python-programs/Marks.py
@@ -1,19 +1,22 @@
-marks = int(input("Enter Your marks : "))
+name = input("Enter You Name : ")
+cls  = input("Enter Your Class : ")
+marks1 = int(input("Enter Your subject 1 marks : "))
+marks2 = int(input("Enter Your subject 2 marks : "))
+marks3 = int(input("Enter Your subject 3 marks : "))
+total = marks1+marks2+marks3
+marks = total/3;
 
-if marks<=100:
-    if marks >= 85:
-        print("You Got Distinction.")
+if marks >= 85:
+    print(f"You Got Distinction {marks}.")
 
-    elif marks >= 65:
-        print("You passed in first class.")
+elif marks >= 65:
+    print(f"You passed in first class {marks}.")
 
-    elif marks >=50:
-        print("You passed in second class.")
+elif marks >=50:
+    print(f"You passed in second class {marks}.")
 
-    elif marks >= 35:
-        print("You are Just pass.")
+elif marks >= 35:
+    print(f"You are Just pass {marks}.")
 
-    else:
-        print("You are failed.")
 else:
-    print("Enter Valid Input.")
+    print(f"You are failed {marks}.")
