f0aea3501f4e3ec4d89433833d348c998d6efa37
Sachin Kudatini <cksachin94@gmail.com>
2026-08-30 08:48:46 +0530

    commit 9

--- programs/SmallestElement.java
+++ programs/SmallestElement.java
@@ -1,3 +1,12 @@
 public class SmallestElement {
-    
+    public static void main(String[] args){
+        int arr[] = {1,5,3,34,32,4,6};
+        int small = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<small){
+                small = arr[i];
+            }
+        }
+        System.out.println("The Smallest Element in the array : "+small);
+    }
 }
